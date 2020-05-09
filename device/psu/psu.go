5fb0c22183733b03f85062c00651a831bee16ff1
renthraysk <renthraysk@gmail.com>
2020-05-09 20:43:13 +0100

    Shift to single psu event with time

--- device/psu/psu.go
+++ device/psu/psu.go
@@ -1,59 +1,34 @@
 package psu
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"math"
 	"math/rand"
 	"time"
 
-	"github.com/renthraysk/webosd/conn"
 	"github.com/renthraysk/webosd/eventsource"
 )
 
 type Event struct {
+	time  time.Time
 	volts float64
 	amps  float64
 }
 
-func NewEvent(volts, amps float64) eventsource.Event {
-	return Event{volts: volts, amps: amps}
+func NewEvent(time time.Time, volts, amps float64) eventsource.Event {
+	return Event{time: time, volts: volts, amps: amps}
 }
 
 func (e Event) WriteTo(w io.Writer) (int64, error) {
-	n, err := fmt.Fprintf(w, "event: volts\ndata: %.3f\n\nevent: amps\ndata: %.3f\n\n", e.volts, e.amps)
+	n, err := fmt.Fprintf(w, "event: psu\ndata: {\"time\": %d, \"volts\": %f, \"amps\": %f}\n\n", e.time.UnixNano()/int64(time.Millisecond), e.volts, e.amps)
 	return int64(n), err
 }
 
 var _ eventsource.Event = (*Event)(nil)
 
-type Commands interface {
-	QueryVoltage(b *bytes.Buffer) error
-	QueryCurrent(b *bytes.Buffer) error
-}
-
 type PSU interface {
 	Poll(t time.Time) eventsource.Event
-}
-
-type psu struct {
-	conn     conn.Conn
-	commands Commands
-}
-
-func New(conn conn.Conn, commands Commands) PSU {
-	return &psu{conn: conn, commands: commands}
-}
-
-func (p *psu) Poll(t time.Time) eventsource.Event {
-	if _, err := p.conn.WriteCommand(p.commands.QueryVoltage); err != nil {
-		return eventsource.NewEventFromError(err)
-	}
-	if _, err := p.conn.WriteCommand(p.commands.QueryCurrent); err != nil {
-		return eventsource.NewEventFromError(err)
-	}
-	return NewEvent(1.0, 1.0)
 }
 
 type fake struct {
@@ -62,7 +37,7 @@ type fake struct {
 func Fake() PSU { return &fake{} }
 
 func (fake) Poll(t time.Time) eventsource.Event {
-	return NewEvent(11.75+rand.Float64(), 1.75+rand.Float64()/2)
+	return NewEvent(t, 11.75+rand.Float64(), 1.75+rand.Float64()/2)
 }
 
 type sin struct {
@@ -73,5 +48,5 @@ func Sin() PSU { return &sin{} }
 
 func (s *sin) Poll(t time.Time) eventsource.Event {
 	s.x += 1.0 / 20
-	return NewEvent(11+math.Sin(s.x)*4, 2+math.Cos(s.x))
+	return NewEvent(t, 11+math.Sin(s.x)*4, 2+math.Cos(s.x))
 }
